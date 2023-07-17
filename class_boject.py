7ded67fa59067c0e36246e0ed4e2c3b169d09656
rpr r <rprr@rprs-MacBook-Pro.local>
2023-07-17 08:50:55 +0545

    new

--- class_boject.py
+++ class_boject.py
@@ -1,8 +1,10 @@
 class HumanBeing():
-    greet ="Welcome ro the program"
+    greet ="Welcome to the program"
+    def __init__(self,name):
+        self.name=name
     def greeting(self):
-        print(self.greet)
+        print(f'{self.greet}, {self.name}')
         
-bibidh_object = HumanBeing()
+bibidh_object = HumanBeing("Bibidh")
 bibidh_object.greeting()
         
