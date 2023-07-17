221eb850681e4867fcd08d7a3cccef5948afe1b0
rpr r <rprr@rprs-MacBook-Pro.local>
2023-07-17 08:30:54 +0545

    new

---
+++ class_boject.py
@@ -0,0 +1,8 @@
+class HumanBeing():
+    greet ="Welcome ro the program"
+    def greeting(self):
+        print(self.greet)
+        
+bibidh_object = HumanBeing()
+bibidh_object.greeting()
+        
