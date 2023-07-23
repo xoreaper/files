d62e34cd55616126715e910a892a14e8808a072e
xoreaper <you@example.com>
2023-07-23 21:05:57 +0545

    hello

--- inheritance.py
+++ inheritance.py
@@ -1,12 +1,24 @@
 class Mammal:
-    def __nit__(self, legs, eyes):
-        self.legs = legs
+    def __init__(self, grade, eyes, name):
+        self.grade = grade
         self.eyes = eyes
+        self.name = name
+
+    def greeting(self):
+        print("Hello , This is a brief about myself cv")
+
+
+class Humanbeing(Mammal):
+    def __init__(self, brain, hands, grade, eyes, name):
+        self.brain = brain
+        self.hands = hands
+        super().__init__(grade, eyes, name)
 
     def print_details(self):
-        print(self.legs, self.eyes)
+        print(
+            f'Name is : {self.name} ,I have {self.eyes} eyes, I study in grade {self.grade}, I have an IQ of  {self.brain} , I have {self.hands} hands')
 
-        class Humanbeing(Mammal):
-            def __init__(self, brain, hands):
-                self.brain = brain
-                self.hands = hands
+
+bibidh_object = Humanbeing("100", "2", "12", "2", "Bibidh")
+bibidh_object.print_details()
+bibidh_object.greeting()
